08f439c7f2154fcbbd4af2ae0b0e0b043efc7771
Martin Crossley <dev@crossleys.biz>
2026-08-31 15:11:34 +0100

    spike detection with sliding integral

--- main.py
+++ main.py
@@ -1,49 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
-
-def find_spikes(samples, samples_per_sec, spike_centile=0.99, spike_width_ms=2, debug=False):
-    # estimate level of spikes (top centile of samples)
-    abs_samples = np.abs(samples)
-    spike_level = np.quantile(abs_samples, spike_centile)
-    if debug:
-        print(f'{spike_centile*100}th centile {spike_level:.3f}')
-
-    # find spikes
-    spike_index = []
-    i = 0
-    while i < len(samples):
-        while i < len(samples) and abs_samples[i] < spike_level:
-            i += 1
-        if i < len(samples):
-            spike_index.append(i)                           # found the start of a spike
-            i += int(samples_per_sec * spike_width_ms/1000) # skip over spike oscillations
-    if debug:
-        print(len(spike_index), 'probable spikes')
-
-    # find most popular interval between spikes
-    spike_interval = np.argmax(np.bincount(np.diff(spike_index)))
-
-    plt.figure()
-    plt.plot(np.bincount(np.diff(spike_index)))
-
-    if debug:
-        print(f'most popular interval {spike_interval} ({samples_per_sec/spike_interval:0.3f}Hz)')
-
-    # find most popular phase of spikes
-    offset = [spike % spike_interval for spike in spike_index]
-    spike_offset = np.argmax(np.bincount(offset))
-    if debug:
-        print(f'most popular offset {spike_offset}')
-    return [spike_interval, spike_offset]
     
 
 def main():
-    #sample_file = 'MSF_9am_3.txt'
-    #sample_rate = 240_000
-    sample_file = 'MSF_10pm_1.txt'
-    sample_rate = 250_000
-    debug=True
+    debug = True
+    use_night = True
+
+    if use_night:
+        sample_file = 'MSF_10pm_1.txt'
+        sample_rate = 250_000
+    else:
+        sample_file = 'MSF_9am_3.txt'
+        sample_rate = 240_000
 
     # read samples from file
     samples = np.loadtxt(sample_file)
@@ -59,10 +28,18 @@ def main():
         last_index = int(sample_end_ms/1000 * sample_rate)
     else:
         last_index = num_samples
-    if (first_index > 0 or last_index > 0):
+    if (sample_start_ms > 0 or sample_end_ms > 0):
         samples = samples[first_index: last_index]
         num_samples = len(samples)
-        print(f'using samples {sample_start_ms}ms - {sample_end_ms}ms)')
+        print(f'using samples {sample_start_ms}ms - {sample_end_ms})')
+
+    # decimate if required
+    decimation = 2
+    if decimation > 1:
+        samples = samples[0::decimation]
+        num_samples = len(samples)
+        sample_rate /= decimation
+
 
     # remove bias
     samples -= np.mean(samples)
@@ -73,66 +50,69 @@ def main():
     t = np.linspace(sample_period, T, num_samples)
     t_ms = 1000 * t
 
-    # find spikes
-    spike_interval, spike_offset = find_spikes(samples, sample_rate, debug=debug)
-    spike_index = range(spike_offset, num_samples, spike_interval)
+    # calculate sliding integral of energy in a period
+    energy = np.power(samples, 2)
+    bin_ms = 2.5
+    binsz = int(bin_ms/1000 * sample_rate)
 
-    # create time mask for signal
-    # note: using a rectangular mask like this adds 100Hz artifacts to the signal - a smoother window would reduce that
-    spike_duration_ms = 5.0     # how long to wait before enabling the mask (normally 3 - 7ms)
-    pre_spike_ms = 0.1          # how soon before the next spike to disable the mask (normally 0.1ms)
-    mask = np.zeros_like(t)
-    mask_first_index = int(spike_duration_ms/1000 * sample_rate)
-    mask_last_index = spike_interval - int(pre_spike_ms/1000 * sample_rate)
-    for index in spike_index:
-        mask[index + mask_first_index: index + mask_last_index] = 1
-
-    if (debug):
-        plt.figure()
-        plt.plot(t_ms, samples, label='signal')
-        plt.plot(t_ms, -200 + 400*mask, label='mask')
-        plt.xlabel('time ms')
-        plt.legend()
-        plt.title('Spike mask')
-
-    masked_signal = samples * mask
-    if debug:
-        plt.figure()
-        plt.plot(t_ms, masked_signal)
-        plt.xlabel('time ms')
-        plt.title('Sampled signal')
-
-    plt.show()
-    return
+    power = np.zeros_like(t)
+    s = sum(energy[0: binsz])
+    for i in range(binsz, num_samples - 1):
+        power[i] = s
+        s -= energy[i - binsz]
+        s += energy[i + 1]
 
     if debug:
-        # show spectrum of masked signal
-        # note: the spikes at 60.100, 60.200 etc are artifacts due to the abrupt edges of the mask (see above)
         plt.figure()
-        plt.magnitude_spectrum(masked_signal, Fs=sample_rate/1000, scale='dB')
-        plt.xlabel('Freq (kHz)')
-        #plt.ylim(-80,-20)
-        if (sample_start_ms > 0 or sample_end_ms > 0):
-            plt.title(f'Signal with spikes masked ({sample_start_ms} - {sample_end_ms}ms)')
-        else:
-            plt.title(f'Signal with spikes masked')
-        plt.grid(linestyle=':')
+        plt.plot(t_ms, samples)
+        plt.plot(t_ms, power / 200e3)
+        plt.title(f'sliding integral of energy ({bin_ms}ms)')
+        plt.xlabel('time (ms)')
 
-    # try a sliding sum of the absolute signal
-    abs_masked_signal = np.abs(masked_signal)
-    detector_output = np.zeros_like(t)
 
-    integration_time_ms = 100
-    integration_num_samples = int(integration_time_ms/1000 * sample_rate)
-    print(integration_num_samples)
-    for i in range(num_samples - integration_num_samples):
-        result = sum(abs_masked_signal[i: i + integration_num_samples: 10])
-        detector_output[i] = result
+    # find first peak (will be bin_ms after the start of the first spike)
+    spike_window_ms = 12.5
+    spike_window = int(spike_window_ms / 1000 * sample_rate)
+    peak_index = 0
+    peak = 0
+    for i in range(0, spike_window):
+        if power[i] > peak:
+            peak = power[i]
+            peak_index = i
+
+    print(f'first spike {peak_index * sample_period * 1000:0.3f}ms')
+
+    # integrate energy over window after each spike
+    data_start_ms = 3.5     # 5 is a good conservative choice
+    data_stop_ms = 7
+    data_start = int(data_start_ms / 1000 * sample_rate)
+    data_stop = int(data_stop_ms / 1000 * sample_rate)
+    data = []
+    i = peak_index
+    data_plot = np.zeros_like(t)
+    while i + data_stop < num_samples:
+        x = sum(energy[i + data_start: i + data_stop])
+        data.append(x)
+        data_plot[i + data_start: i + data_stop] = x
+
+        # find next peak
+        peak_search_start_ms = 9
+        peak_search_stop_ms = 11
+        peak_search_start = int(peak_search_start_ms / 1000 * sample_rate)
+        peak_search_stop = int(peak_search_stop_ms / 1000 * sample_rate)
+        peak = 0
+        peak_index = 0
+        for j in range(i + peak_search_start, i + peak_search_stop):
+            if power[j] > peak:
+                peak = power[j]
+                peak_index = j
+        i = peak_index
 
     plt.figure()
-    plt.plot(t_ms, masked_signal)
-    plt.plot(t_ms, detector_output)
-    plt.title(f'Sliding {integration_time_ms}ms sum of signal envelope')
+    plt.plot(t_ms, samples)
+    plt.plot(t_ms, data_plot/1e4)
+    plt.title('Detector output')
+    plt.xlabel('time (ms)')
 
     plt.show()
 
